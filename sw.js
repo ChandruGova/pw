f28728373c3bde18d8618e211841660ed6c27500
chandrugova <chandru@vknowlabs.com>
2020-02-03 14:47:42 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('push', event => {
-    const data = event.data.json()
-    console.log('New notification', data)
-    const options = {
-      body: data.body,
-    }
-    event.waitUntil(
-      self.registration.showNotification(data.title, options)
+    // const data = event.data.json()
+    console.log('New notification', event)
+    // const options = {
+    //   body: data.body,
+    // }
+    // event.waitUntil(
+    //   self.registration.showNotification(data.title, options)
     
-      );
+    //   );
   })
