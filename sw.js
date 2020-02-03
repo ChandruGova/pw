6fad0b45bbc55e4132e2fca49e8309c3b3f8f0c6
chandrugova <chandru@vknowlabs.com>
2020-02-03 14:55:35 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
 self.addEventListener('push', event => {
     // const data = event.data.json()
+    if(event && event.data ){
+        console.log(event.data,"dddddddddd")
+    }
     console.log('New notification', event)
     // const options = {
     //   body: data.body,
