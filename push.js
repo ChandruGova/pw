d15d4a9a6d89f57b14fc12c0394230d161dd3e5c
chandrugova <chandru@vknowlabs.com>
2020-02-03 14:44:30 +0530

    fix

--- push.js
+++ push.js
@@ -8,7 +8,7 @@ var vapidKeys = {
 
 push.setVapidDetails('mailto:chandru@vknowlabs.com' , vapidKeys.publicKey , vapidKeys.privateKey);
 
-let sub =  {"endpoint":"https://fcm.googleapis.com/fcm/send/duQaOqSJ9Jo:APA91bHXiKODq71U6bwYwW7qYK31EOov7aAPDk7TBDD2Y9_YVxso2cVY2fAfwjN4n_VXKIXTAtV_1eZtLj7YnlNTGpqkJq7C0kf6KBSug4QjUuZtRgPdMgU5z7eZ_a2dONqkErKdY0_B","expirationTime":null,
-"keys":{"p256dh":"BKPuvplinN_hxv-VISGaBlu5RUuwzJWFtkp9Wc_f6vGtkjgbuZQiHiNzMDOQkSL7o52Y68JbuMNkCuhZj4Kgijw","auth":"HCfcvEmzsFgDfFZzwhkvfQ"}}
+let sub =  {"endpoint":"https://fcm.googleapis.com/fcm/send/efexhO7Elqs:APA91bEvVi35finq95P7OPUprATesTDdgWo3lTEC5Sa3hO8JGgBs-nHdztjKg9pJMbozLOSD-NLC-mwZ6azCPw61tI24i24XTm7mKhir80gB_pMxrQ3j8EKMs285yZo8Vd_PVyW9ZGNE","expirationTime":null,
+"keys":{"p256dh":"BFbReEc9TNwNkAw_UCjclPZmj6gbquF-KqyjxLY3_Pyva0OCUOUNLt3B2ZNUqlYqDLyud5hQIJT8Qu-qMA22R4w","auth":"qQAu_8VfipRg4EQxmNX8RQ"}}
 
 push.sendNotification(sub , "test email")
