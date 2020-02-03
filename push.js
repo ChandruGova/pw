7299f743a1aa476144f805defaad57d4977e4d5c
chandrugova <chandru@vknowlabs.com>
2020-02-03 12:58:01 +0530

    initial

---
+++ push.js
@@ -0,0 +1,13 @@
+var push = require('web-push');
+
+
+var vapidKeys = {
+    publicKey:'BFUW60E9Rw0doM1FZ-pgED2WBJM8sC2oOiWv8OomS5fSloPkB9nO6uGxHEr4WvklSwl3LQ4HuBZKKdZPKaCC8sk',
+    privateKey: '4mp669vmNy4YKLAcPpqrOhqkivEx06wg16JeazZholY' 
+};
+
+push.setVapidDetails('mailto:chandru@vknowlabs.com' , vapidKeys.publicKey , vapidKeys.privateKey);
+
+let sub = {}
+
+push.sendNotification(sub , "test email")
