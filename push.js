7b53f93e413f88fc70aaac7b04adabf084ad78c6
chandrugova <chandru@vknowlabs.com>
2020-02-03 15:15:10 +0530

    fix

--- push.js
+++ push.js
@@ -8,12 +8,12 @@ var vapidKeys = {
 
 push.setVapidDetails('mailto:chandru@vknowlabs.com' , vapidKeys.publicKey , vapidKeys.privateKey);
 
-let sub =  {"endpoint":"https://fcm.googleapis.com/fcm/send/d3e_TPmZYQE:APA91bESrtdH6nAE_Ucjpto4SCghXHq9SxqwEVuJRq4hEDnL8Bz-gs_h87GUai87MQRC8M1kXpqLHw5QGs_Pcc00OkfckVp1zyOIbRuyh1rYtPsSeJmYWFMJfWNRcxau-q-ShcwnTvjz","expirationTime":null,"keys":{"p256dh":"BN2tWQzmYmkVNTTzphcRFhhoOZR1EZIhQtLhxxmE_aZ0vk-6_WI8lwe4takJ4YnKdawMYsqQyOAyIXcM4Zhmh_k","auth":"3fmz4OxXpcazV-Yr1SLaAg"}}
+let sub =  {"endpoint":"https://fcm.googleapis.com/fcm/send/d_uvycsPDvw:APA91bGEemaTbBnvRPeiPdxNJlpVsd4cqKnnREGSrPMubPc85PWbTCN8A-YT6UpF-jL_-sfzqJeKzf14xcxOZQ3GiR5wrmk_G-95HEHY19ue0_nH8X8ulUiLCzaLv1G2dZluLQ5VvWHG","expirationTime":null,"keys":{"p256dh":"BFiCIcECvbtdlDyghArM1gp0ttVz4LkRfwJCcMuo2OJMEdlTOvh1lRhAp1DNpCb-zC7t0dZBnLnhBP5f-3_NdNc","auth":"hSAbTfDQPKPaEL0GxU4vjg"}}
 const notificationPayload = JSON.stringify({
     "notification": {
         "title": "Angular News",
         "body": "Newsletter Available!",
-        "icon": "assets/main-page-logo-small-hat.png",
+        // "icon": "assets/main-page-logo-small-hat.png",
         "vibrate": [100, 50, 100],
         "data": {
             "dateOfArrival": Date.now(),
