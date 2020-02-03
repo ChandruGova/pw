cb5f3d9ca668b63bd4e552c8addd2063eab8fdfd
chandrugova <chandru@vknowlabs.com>
2020-02-03 13:11:54 +0530

    fix

--- push.js
+++ push.js
@@ -8,6 +8,7 @@ var vapidKeys = {
 
 push.setVapidDetails('mailto:chandru@vknowlabs.com' , vapidKeys.publicKey , vapidKeys.privateKey);
 
-let sub = {}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cnYie_9PcvA:APA91bEhmYiSh6dAbrcWMu6ZA3-E0bUy3JreYBCuNJ7cWvV_CyE7uc6bEpBD0r8wm2ZeZgqg3VC068KL5wkyuFM3EEKtshUyVVWl63W0lonP1jnW8AKdtyO812lnAaa0Vck2kJc7X61a","expirationTime":null,
+"keys":{"p256dh":"BKwrQypshRtkXsDolw_ztpmqaTNb58RS0CHXn3u_ZLgYoem6NTj7qUkvEYG6GKmbju2cXiRgu_M3vOtFGsXDyq0","auth":"s7Bqm0smOQytWrKsnsC7ww"}}
 
 push.sendNotification(sub , "test email")
